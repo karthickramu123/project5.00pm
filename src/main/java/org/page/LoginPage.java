072580d317771124421b0bb7a243fe722fa483fd
karthickramu123 <98465745+karthickramu123@users.noreply.github.com>
2022-01-28 22:07:11 +0530

    karthick update code

--- src/main/java/org/page/LoginPage.java
+++ src/main/java/org/page/LoginPage.java
@@ -34,7 +34,8 @@ public class LoginPage extends BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("karthik");
+System.out.println("suren");
 	}
 
 }
